b4fe72417050b3b06a5d34e937d04a0c7404f8dc
iskugor <ivan.skugor@gmail.com>
2012-07-31 14:08:14 +0200

    iOS specific error reporting

--- app/Resources/api/Log.js
+++ app/Resources/api/Log.js
@@ -1,24 +1,32 @@
 ['info','error','debug','trace','warn','pass','fail','test'].forEach(function(level){
 	if (level == 'error') {
 		exports[level] = function(e) {
-
-			var temp = e.stack.split("\n");
-	
-			var myerror = {};
-	
-			for (var i = 0; i < 2; i++) {
-				temp[i] = temp[i].split(":");
+			var message;
+			if (!Ti.UI.Android) {
+				message = "Line " + e.line + ": " + e.message;
 			}
-			myerror.message = temp[0][1].substr(1);
-			myerror.file = temp[1][0].substr(7);
-			myerror.line = temp[1][1];
+			else {
+				var temp = e.stack.split("\n");
 	
-			var message = "Line: " + myerror.line + "\n" + "File: " + myerror.file + "\n" + "Message: " + myerror.message;
-	
+				var myerror = {};
+		
+				for (var i = 0; i < 2; i++) {
+					temp[i] = temp[i].split(":");
+				}
+				myerror.message = temp[0][1].substr(1);
+				myerror.file = temp[1][0].substr(7);
+				myerror.line = temp[1][1];
+		
+				message = "Line: " + myerror.line + "\n" + "File: " + myerror.file + "\n" + "Message: " + myerror.message;
+		
+				
+			}
+			
 			Ti.App.fireEvent("tishadow:socket_log", {
 				level : level.toUpperCase(),
 				message : message
 			});
+			
 		};
 	}
 	else {
