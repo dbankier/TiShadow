4d89bf581b13cc9238cfc118886cb9bc25b23cd5
iskugor <ivan.skugor@gmail.com>
2012-08-01 12:29:19 +0200

    Revert old Log.js file

--- app/Resources/api/Log.js
+++ app/Resources/api/Log.js
@@ -1,45 +1,9 @@
 ['info','error','debug','trace','warn','pass','fail','test'].forEach(function(level){
-	if (level == 'error') {
-		exports[level] = function(e) {
-			var message;
-			if (!Ti.UI.Android) {
-				message = "Line " + e.line + ": " + e.message;
-			}
-			else {
-				var temp = e.stack.split("\n");
-	
-				var myerror = {};
-		
-				for (var i = 0; i < 2; i++) {
-					temp[i] = temp[i].split(":");
-				}
-				myerror.message = temp[0][1].substr(1);
-				myerror.file = temp[1][0].substr(7);
-				myerror.line = temp[1][1];
-		
-				message = "Line: " + myerror.line + "\n" + "File: " + myerror.file + "\n" + "Message: " + myerror.message;
-		
-				
-			}
-			
-			Ti.App.fireEvent("tishadow:socket_log", {
-				level : level.toUpperCase(),
-				message : message
-			});
-			
-		};
-	}
-	else {
-		exports[level] = function(message) {
-
-			Ti.App.fireEvent("tishadow:socket_log", {
-				level : level.toUpperCase(),
-				message : message
-			});
-		}; 
-	}
-  
-	
-
+  exports[level] = function(message) {
+    Ti.App.fireEvent("tishadow:socket_log", {
+      level: level.toUpperCase(),
+      message: message
+    });
+  };
 });
 
