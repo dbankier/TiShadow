fbce96583d8db678f2220047f476470a01025782
David Bankier <dbankier@hotmail.com>
2012-12-02 16:02:38 +1100

    minor ruble updates

--- ruble/commands/commands.rb
+++ ruble/commands/commands.rb
@@ -1,20 +1,20 @@
 require 'ruble'
 
 command 'Full TiShadow Build' do |cmd|
-  cmd.key_binding = 'CTRL+S'
+  cmd.key_binding = 'SHIFT+CTRL+S'
   cmd.output = :output_to_console
   cmd.working_directory = :current_project
-  cmd.invoke = "tishadow"
+  cmd.invoke = "tishadow run"
 end
 
 command 'TiShadow Build Update' do |cmd|
-  cmd.key_binding = 'SHIFT+CTRL+S'
+  cmd.key_binding = 'CTRL+S'
   cmd.input = :none
   cmd.output = :output_to_console
-  #Auto update on save currently working...
+  #Auto update on save currently not working...
   #cmd.trigger = :execution_listener, "org.eclipse.ui.file.save"
   cmd.working_directory = :current_project
-  cmd.invoke = "tishadow update"
+  cmd.invoke = "tishadow run --update"
 end
 
 command 'Open TiShadow Webpage' do |cmd|
