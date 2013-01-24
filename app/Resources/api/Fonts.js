1c15c10f35fd20cad211a64d22b2e6c9fefcdafb
David Bankier <dbankier@hotmail.com>
2013-01-25 08:34:40 +1100

    Support for fonts in subdirectories, e.g. fonts from Alloy Widgets.

--- app/Resources/api/Fonts.js
+++ app/Resources/api/Fonts.js
@@ -2,15 +2,22 @@ var log = require("/api/Log");
 var TiShadow = require("/api/TiShadow");
 var TiDynamicFont = require('yy.tidynamicfont');
 
-exports.loadCustomFonts = function(name) {
-  var dir = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory + name + "/fonts");
+exports.loadCustomFonts = function(name, base) {
+  base = base || "";
+  var dir = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory + name + "/fonts/" + base);
+  log.debug(dir.nativePath);
   var files = dir.getDirectoryListing();
   if (!files) {
     return;
   }
   files.forEach(function(file_name) {
-    log.debug("Registering Font: " + file_name);
-    var file = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory + name + "/fonts/" + file_name);
-    TiDynamicFont.registerFont(file);
+    if (file_name.toLowerCase().match(/\.otf$/) ||
+       file_name.toLowerCase().match(/\.ttf$/) ) { // assume font
+      log.debug("Registering Font: " + file_name);
+      var file = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory + name + "/fonts/" + base + file_name);
+      TiDynamicFont.registerFont(file);
+    } else { //assume directory (due to no isDirectory in iOS)
+      exports.loadCustomFonts(name, base + file_name + "/");
+    }
   });
 };
