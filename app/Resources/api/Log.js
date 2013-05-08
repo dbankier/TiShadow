ee9a33471449f4fae03c76f6c42949ceec04b89b
Fokke Zandbergen <mail@fokkezb.nl>
2013-05-08 15:40:00 +0200

    Added support for console.* and automatic JSON.stringify on object messages.

--- app/Resources/api/Log.js
+++ app/Resources/api/Log.js
@@ -1,16 +1,24 @@
 
 ['info','error','debug','trace','repl','warn','pass','fail','test'].forEach(function(level){
   exports[level] = function(message) {
+  	_write(level, message);
+  };
+});
+
+exports.log = function(level, message) {
+	if (typeof message === 'undefined') {
+		message = level;
+		level = 'info';
+	}
+	_write(level, message);
+};
+
+function _write(level, message) {
+  	if (typeof message === 'object') {
+  		message = JSON.stringify(message, null, 4);
+  	}
     require("/api/TiShadow").emitLog({
       level: level.toUpperCase(),
       message: message
     });
-  };
-});
-
-exports.log = function(level,message) {
-  if (exports[level]) {
-    exports[level](message);
-  }
 }
-
