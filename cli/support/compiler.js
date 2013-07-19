c2104328566ee3dd241d704f9b9d4af4490b9e92
David Bankier <dbankier@hotmail.com>
2013-07-19 10:54:38 +1000

    fix to catch tab usage when compiling

--- cli/support/compiler.js
+++ cli/support/compiler.js
@@ -25,7 +25,7 @@ function prepare(src, dst, callback) {
       .replace(/Ti(tanium)?.UI.createWindow\(/g, "__ui.createWindow(")
       .replace(/Ti(tanium)?.UI.createTabGroup\(/g, "__ui.createTabGroup(")
       .replace(/Ti.Locale.getString/g, "L")
-      .replace(/([ :=\(\+]+)(['"])(\/[^'"].*?)(['"])/g, function($0, $1, $2, $3, $4) {
+      .replace(/([ \t:=\(\+]+)(['"])(\/[^'"].*?)(['"])/g, function($0, $1, $2, $3, $4) {
           if ($1.indexOf("+") > -1 ) { return $0; } else { return $1 +'__p.file(' +$2 + $3+ $4+ ')'; }
         }) // ignores "/" or if preceeded by plus
       // Replace strings like ".titleid = 'save'" -> "title = L('save')"
