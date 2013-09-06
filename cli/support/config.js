ddea2a4bafe28e623af5d8792f031c216fb8032a
Fokke Zandbergen <mail@fokkezb.nl>
2013-09-06 09:01:01 +0200

    Copy fonts with appify to support custom fonts on Android.

--- cli/support/config.js
+++ cli/support/config.js
@@ -28,6 +28,7 @@ if (fs.existsSync(config_path)) {
 config.base = base;
 config.alloy_path        = path.join(base, 'app');
 config.resources_path    = path.join(base, 'Resources');
+config.fonts_path        = path.join(config.resources_path, 'fonts');
 config.modules_path      = path.join(base, 'modules');
 config.platform_path     = path.join(base, 'platform');
 config.spec_path         = path.join(base, 'spec');
