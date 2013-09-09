c427012af54487d0c9a9a12fe99e49af7d0ed51a
David Bankier <dbankier@hotmail.com>
2013-09-09 10:32:00 +1000

    closes #118

--- app/Resources/api/Fonts.js
+++ app/Resources/api/Fonts.js
@@ -2,10 +2,8 @@ var log = require("/api/Log");
 var TiShadow = require("/api/TiShadow");
 var TiDynamicFont = require('yy.tidynamicfont');
 
-exports.loadCustomFonts = function(name, base) {
-  base = base || "";
-  var dir = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory + name + "/fonts/" + base);
-  log.debug(dir.nativePath);
+function loadAll(root) {
+  var dir = Ti.Filesystem.getFile(root);
   var files = dir.getDirectoryListing();
   if (!files) {
     return;
@@ -14,10 +12,13 @@ exports.loadCustomFonts = function(name, base) {
     if (file_name.toLowerCase().match(/\.otf$/) ||
        file_name.toLowerCase().match(/\.ttf$/) ) { // assume font
       log.debug("Registering Font: " + file_name);
-      var file = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory + name + "/fonts/" + base + file_name);
+      var file = Ti.Filesystem.getFile(root + file_name);
       TiDynamicFont.registerFont(file);
-    } else { //assume directory (due to no isDirectory in iOS)
-      exports.loadCustomFonts(name, base + file_name + "/");
-    }
+    } 
   });
+
+}
+exports.loadCustomFonts = function(name) {
+  loadAll(Ti.Filesystem.applicationDataDirectory + name + "/fonts/");
+  loadAll(Ti.Filesystem.applicationDataDirectory + name + "/iphone/fonts/");
 };
