29d50215a9c82932bc53e918f2f28e5766ff4c16
David Bankier <dbankier@hotmail.com>
2013-09-30 13:19:26 +1000

    manual unstack needs to be called before close for Android.

--- app/Resources/api/UI.js
+++ app/Resources/api/UI.js
@@ -93,10 +93,10 @@ exports.closeApp = function(a) {
   if (app && containers[app]) {
     for (var c in containers[app]) {
       if (containers[app].hasOwnProperty(c)) {
-        containers[app][c][containers[app][c].__closeFn || 'close']();
         if (containers[app][c].__tishadowDumb) {
           unstack({app:app, container:container});
         }
+        containers[app][c][containers[app][c].__closeFn || 'close']();
       }
     }
   }
