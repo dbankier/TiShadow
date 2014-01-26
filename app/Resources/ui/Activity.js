3b7052b7bbe744c1f43548c87b1feb8b040fd7fe
David Bankier <dbankier@hotmail.com>
2014-01-27 09:47:48 +1100

    One activity - android changes

--- app/Resources/ui/Activity.js
+++ app/Resources/ui/Activity.js
@@ -1,17 +1,12 @@
-/*globals exports*/
-/*
- * CAREFUL: I'm trying to start using CommonJS modules. This is one.
- */
-
 var activity_win, activity_view, activity_label;
 var osname = Ti.Platform.osname;
-var screenHeight = Ti.Platform.displayCaps.platformHeight;
-var screenWidth = Ti.Platform.displayCaps.platformWidth;
 
 function Activity(message) {
 	activity_win = Titanium.UI.createWindow({
 		backgroundColor : '#000',
 		top : 0,
+    fullscreen: true,
+    navBarHidden: true,
 		bottom : 0,
 		left : 0,
 		right : 0,
@@ -28,7 +23,7 @@ function Activity(message) {
 
 	var activity_indicator = Titanium.UI.createActivityIndicator({
 		color : '#fff',
-		style : Titanium.UI.iPhone.ActivityIndicatorStyle.BIG,
+		style : Ti.Platform.osname === "android" ?  Titanium.UI.ActivityIndicatorStyle.BIG : Titanium.UI.iPhone.ActivityIndicatorStyle.BIG,
 		height : 20,
 		width : 20,
 		left : 25
@@ -44,34 +39,15 @@ function Activity(message) {
 	});
 	activity_view.add(activity_label);
 	activity_view.add(activity_indicator);
-	activity_indicator.show();
+  activity_indicator.show();
 	activity_win.add(activity_view);
 };
 
 Activity.prototype.show = function(win) {
-	activity_view.show();
-	if(win == null) {
-		activity_win.height = screenHeight;
-		activity_view.opacity = 1;
-		activity_win.open();
-	} else {
-		this.coverView = Ti.UI.createView();
-		activity_view.opacity = 0.90;
-		activity_view.bottom = win.height * 0.48;
-		win.add(this.coverView);
-		win.add(activity_view);
-	}
+  activity_win.open();
 };
-Activity.prototype.hide = function(win) {
-	activity_view.hide();
-	if(win == null) {
-		activity_win.height = 0;
-		activity_win.close();
-	} else {
-		win.remove(this.coverView);
-		this.coverView = null;
-		win.remove(activity_view);
-	}
+Activity.prototype.hide = function() {
+	activity_win.close();
 };
 Activity.prototype.setMessage = function(text) {
 	activity_label.text = text;
