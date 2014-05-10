bfa7169d36d7706164804ac49bb9c630a2dd5de3
David Bankier <dbankier@hotmail.com>
2014-05-11 09:20:40 +1000

    closes #279 - avoids ipselector if host set using, e.g. `ts config --host 192.168.22.22`

--- plugin/yy.tishadow/1.0/hooks/shadow.js
+++ plugin/yy.tishadow/1.0/hooks/shadow.js
@@ -10,6 +10,7 @@ var path = require('path');
 var fs = require('fs');
 var commands = require('../commands/shadow');
 var ipselector = require('ipselector');
+var home = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
 
 exports.cliVersion = '>=3.2.0';
 exports.version = '1.0';
@@ -31,16 +32,16 @@ function init(_logger, config, cli) {
 }
 function preCompileHook(isExpress) {
   return function (build, finished) {
+    // temp appify build path
     var new_project_dir = path.join(build.projectDir, 'build', 'appify');
 
-    // get ip address
-    var ip_address = ipselector.selectOne({
-      family : 'IPv4',
-      internal : false
-    },function(ip_address){
-      var args = build.cli.argv.$_
-           .filter(function(el) { return el !== "--shadow" && el !== "--tishadow" && el !== "--appify"})
-           .concat(["--project-dir", new_project_dir]);
+    // pass through arguments
+    var args = build.cli.argv.$_
+               .filter(function(el) { return el !== "--shadow" && el !== "--tishadow" && el !== "--appify"})
+               .concat(["--project-dir", new_project_dir]);
+
+    // appify -> express
+    function launch(ip_address){
       commands.startAppify(logger, new_project_dir, build.cli.argv.platform, ip_address, function() {
         commands.buildApp(logger,args)
         if (isExpress) {
@@ -48,6 +49,26 @@ function preCompileHook(isExpress) {
           commands.startWatch(logger, build.cli.argv.platform, ip_address);
         }
       });
-    });
+    }
+
+    // existing tishadow config?
+    var config;
+    var config_path = path.join(home,'.tishadow.json');
+    if (fs.existsSync(config_path)) {
+      config = require(config_path);
+    }
+
+    if (config && config.host) {
+      launch(config.host);
+    } else {
+      // get ip address
+      var ip_address = ipselector.selectOne({
+        family : 'IPv4',
+        internal : false
+      },function(ip_address) {
+        launch(ip_address);
+      });
+    }
   }
 }
+
