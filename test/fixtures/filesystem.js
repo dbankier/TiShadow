2aa87e6f73183b32e8814c3ac28c5d3f3e13de1f
Guillermo Zunino <guillez@gmail.com>
2014-08-15 14:15:10 -0300

    Changed uglify of Ti.Filesystem.getFile to use a new __p.getFile method which does proper handling of '/' to be API compatible with Titanium getFile. Updated filesystem tests to reflect change and more coverage

--- test/fixtures/filesystem.js
+++ test/fixtures/filesystem.js
@@ -9,3 +9,9 @@ Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, "sounds", "my.wav");
 Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, "sounds", "music", "my.wav");
 Ti.Filesystem.getResourcesDirectory();
 Ti.Filesystem.resourcesDirectory + "sounds/my.wav";
+
+Ti.Filesystem.getFile("/appdata-private://testApp/","/testDir/");
+Ti.Filesystem.getFile("appdata-private://testApp/","/testDir/");
+Ti.Filesystem.getFile("appdata-private://testApp","/testDir/");
+Ti.Filesystem.getFile("appdata-private://testApp","testDir/");
+Ti.Filesystem.getFile("appdata-private://testApp","testDir");
