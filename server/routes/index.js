e68eca9cbba3adec66ca9cb60cf9ca4362e8d662
David Bankier <dbankier@hotmail.com>
2017-05-29 08:45:43 +1000

    Catch bad data

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,35 +4,33 @@
  */
 
 var fs = require('fs'),
-    path = require('path'),
-    Logger = require('../logger'),
-    sockets = require('../support/sockets'),
-    rooms = require('../support/rooms'),
-    config = require('../../cli/support/config');
+  path = require('path'),
+  Logger = require('../logger'),
+  sockets = require('../support/sockets'),
+  rooms = require('../support/rooms'),
+  config = require('../../cli/support/config');
 var multiparty = require('multiparty');
-
 
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
+exports.index = function(req, res) {
   res.render('index', { title: 'TiShadow' });
 };
-exports.screencast = function(req, res){
+exports.screencast = function(req, res) {
   res.render('screencast', { title: 'TiShadow' });
 };
 
-exports.api = function (req, res) {
-  res.sendfile(path.resolve(__dirname + "/../../lib/api.jsca"));
+exports.api = function(req, res) {
+  res.sendfile(path.resolve(__dirname + '/../../lib/api.jsca'));
 };
 
-
 // Bundles handled by GET/POST instead of socket connections.
-exports.getBundle = function(req,res) {
-  Logger.debug("Bundle requested." );
+exports.getBundle = function(req, res) {
+  Logger.debug('Bundle requested.');
   res.setHeader('Content-disposition', 'attachment; filename=bundle.zip');
-  res.setHeader('Content-type', "application/zip");
+  res.setHeader('Content-type', 'application/zip');
 
   var filestream = fs.createReadStream(rooms.get(req.params.room).bundle);
   filestream.on('data', function(chunk) {
@@ -42,7 +40,7 @@ exports.getBundle = function(req,res) {
     res.end();
   });
   filestream.on('error', function(exception) {
-      Logger.error(exception);
+    Logger.error(exception);
   });
 };
 
@@ -50,13 +48,20 @@ exports.getBundle = function(req,res) {
 exports.postBundle = function(req, res) {
   var form = new multiparty.Form();
   form.parse(req, function(err, fields, files) {
-    Logger.log("WARN", null, "Remote Bundle Received");
-    var data = JSON.parse(fields.data[0]),
-        name = files.bundle[0].originalFilename.replace(".zip",""),
-        room = data.room;
+    Logger.log('WARN', null, 'Remote Bundle Received');
+    var data;
+    try {
+      data = JSON.parse(fields.data[0]);
+    } catch (e) {
+      Logger.error('Bad Data');
+      Logger.error(fields);
+      return;
+    }
+    var name = files.bundle[0].originalFilename.replace('.zip', ''),
+      room = data.room;
     rooms.addBundle(room, name, files.bundle[0].path);
     var curr = rooms.get(room);
-    Logger.log("INFO", null, "New Bundle: " + curr.bundle + " | " + name);
+    Logger.log('INFO', null, 'New Bundle: ' + curr.bundle + ' | ' + name);
 
     data.name = name;
     data.room = data.bundle = null;
@@ -64,10 +69,8 @@ exports.postBundle = function(req, res) {
       data.version = curr.version;
     }
     if (!data.deployOnly) {
-      sockets.emit(room, "bundle", data);
+      sockets.emit(room, 'bundle', data);
     }
-    res.send("OK", 200);
+    res.send('OK', 200);
   });
 };
-
-
