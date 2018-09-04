d6d8822e888bf5447b3ffb23937bc4344c5389f9
David Bankier <dbankier@hotmail.com>
2018-09-04 19:34:55 +1000

    container app updated - for those that use it...

--- app/Resources/ui/Activity.js
+++ app/Resources/ui/Activity.js
@@ -7,61 +7,61 @@ var activity_win, activity_view, activity_label;
 var osname = Ti.Platform.osname;
 
 function Activity(message) {
-	activity_win = Titanium.UI.createWindow({
-		backgroundColor : '#000',
-		top : 0,
+  activity_win = Titanium.UI.createWindow({
+    backgroundColor: '#000',
+    top: 0,
     fullscreen: true,
     navBarHidden: true,
-		bottom : 0,
-		left : 0,
-		right : 0,
-		opacity : 0.70,
-		theme:"Theme.NoActionBar"
-	});
-	activity_view = Titanium.UI.createView({
-		width : 280,
-		height : 90,
-		backgroundColor : '#000',
-		borderWidth : 2,
-		borderColor : '#999'
-	});
+    bottom: 0,
+    left: 0,
+    right: 0,
+    opacity: 0.7,
+    theme: 'Theme.NoActionBar'
+  });
+  activity_view = Titanium.UI.createView({
+    width: 280,
+    height: 90,
+    backgroundColor: '#000',
+    borderWidth: 2,
+    borderColor: '#999'
+  });
 
-        var actStyle;
-        if (parseInt(Ti.version.substr(0,1))<5){
-            actStyle = Ti.Platform.osname === "android" ?  Titanium.UI.ActivityIndicatorStyle.BIG : Titanium.UI.iPhone.ActivityIndicatorStyle.BIG;
-        } else {
-            actStyle = Titanium.UI.ActivityIndicatorStyle.BIG;
-        }
-	var activity_indicator = Titanium.UI.createActivityIndicator({
-		color : '#fff',
-		style : actStyle,
-		height : 20,
-		width : 20,
-		left : 25
-	});
-	activity_label = Ti.UI.createLabel({
-		left : 70,
-		color : '#fff',
-		font : {
-			fontWeight : 'bold',
-			fontSize : '16'
-		},
-		text : message
-	});
-	activity_view.add(activity_label);
-	activity_view.add(activity_indicator);
+  var actStyle;
+  if (parseInt(Ti.version.substr(0, 1)) < 5) {
+    actStyle = Titanium.UI.ActivityIndicatorStyle.BIG;
+  } else {
+    actStyle = Titanium.UI.ActivityIndicatorStyle.BIG;
+  }
+  var activity_indicator = Titanium.UI.createActivityIndicator({
+    color: '#fff',
+    style: actStyle,
+    height: 20,
+    width: 20,
+    left: 25
+  });
+  activity_label = Ti.UI.createLabel({
+    left: 70,
+    color: '#fff',
+    font: {
+      fontWeight: 'bold',
+      fontSize: '16'
+    },
+    text: message
+  });
+  activity_view.add(activity_label);
+  activity_view.add(activity_indicator);
   activity_indicator.show();
-	activity_win.add(activity_view);
-};
+  activity_win.add(activity_view);
+}
 
 Activity.prototype.show = function(win) {
   activity_win.open();
 };
 Activity.prototype.hide = function() {
-	activity_win.close();
+  activity_win.close();
 };
 Activity.prototype.setMessage = function(text) {
-	activity_label.text = text;
+  activity_label.text = text;
 };
 
 module.exports = Activity;
--- app/Resources/ui/NavBar.js
+++ app/Resources/ui/NavBar.js
@@ -3,36 +3,41 @@
  * Please see the LICENSE file included with this distribution for details.
  */
 
-var connect_button = Ti.UI.createButton({title:'Connect', color: '#2192E3', left: "10dp", width: Ti.UI.SIZE});
+var connect_button = Ti.UI.createButton({
+  title: 'Connect',
+  color: '#2192E3',
+  left: '10dp',
+  width: Ti.UI.SIZE
+});
 
 module.exports = {
   add: function(o) {
     connect_button.addEventListener('click', o.connect);
-    if(Ti.Platform.osname !== "android") {
-      connect_button.style = Ti.UI.iPhone.SystemButtonStyle.DONE;
+    if (Ti.Platform.osname !== 'android') {
+      connect_button.style = Ti.UI.iOS.SystemButtonStyle.DONE;
       var flexSpace = Ti.UI.createButton({
-        systemButton:Titanium.UI.iPhone.SystemButton.FLEXIBLE_SPACE
+        systemButton: Titanium.UI.iOS.SystemButton.FLEXIBLE_SPACE
       });
 
       var title = Ti.UI.createLabel({
-        text:"TiShadow",
-        font:{
-          fontSize:18,
-          fontWeight:'bold'
+        text: 'TiShadow',
+        font: {
+          fontSize: 18,
+          fontWeight: 'bold'
         }
       });
 
       var bar = Ti.UI.iOS.createToolbar({
-        items:[connect_button,flexSpace, title, flexSpace],
-        top:0,
-        barColor:'#f8f8f8',
-        height:40
+        items: [connect_button, flexSpace, title, flexSpace],
+        top: 0,
+        barColor: '#f8f8f8',
+        height: 40
       });
 
       o.win.add(bar);
     } else {
       var view = Ti.UI.createView({
-        height: "50dp",
+        height: '50dp',
         width: Ti.UI.FILL,
         top: 0,
         backgroundColor: '#f8f8f8'
@@ -44,8 +49,7 @@ module.exports = {
   },
   setConnected: function(val) {
     connect_button.connected = val;
-    connect_button.title = val ? "Disconnect" : "Connect";
+    connect_button.title = val ? 'Disconnect' : 'Connect';
     connect_button.width = Ti.UI.SIZE;
   }
-}
-
+};
