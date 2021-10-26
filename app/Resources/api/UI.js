ecc64b4e65605db626fa567af3d73556cccc9ee9
David Bankier <dbankier@hotmail.com>
2021-10-26 17:22:53 +0300

    fix for ti 10, createNavigationWindow; bump 3.2.1

--- app/Resources/api/UI.js
+++ app/Resources/api/UI.js
@@ -3,16 +3,16 @@
  * Please see the LICENSE file included with this distribution for details.
  */
 
-var _ = require("/lib/underscore");
-var log = require("/api/Log");
+var _ = require('/lib/underscore');
+var log = require('/api/Log');
 
 var containers = {};
 
 function stack(e) {
-  var p = require("/api/PlatformRequire");
+  var p = require('/api/PlatformRequire');
   var container = e.source.__tishadowContainer;
   var app = e.source.__tishadowApp;
-  if (require("/api/TiShadow").inspector) {
+  if (require('/api/TiShadow').inspector) {
     p.addSpy(container, e.source);
     log.spy(container);
   }
@@ -24,8 +24,8 @@ function stack(e) {
 }
 
 function unstack(e) {
-  var p = require("/api/PlatformRequire");
-  if (require("/api/TiShadow").inspector) {
+  var p = require('/api/PlatformRequire');
+  if (require('/api/TiShadow').inspector) {
     p.removeSpy(e.container);
   }
   delete containers[e.app][e.container];
@@ -33,8 +33,15 @@ function unstack(e) {
 }
 
 function prepareArgs(a) {
-  var container = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {var r = Math.random()*16|0,v=c=='x'?r:r&0x3|0x8;return v.toString(16);});
-  var app = require("/api/TiShadow").currentApp || '__REPL';
+  var container = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+    /[xy]/g,
+    function (c) {
+      var r = (Math.random() * 16) | 0,
+        v = c == 'x' ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+    }
+  );
+  var app = require('/api/TiShadow').currentApp || '__REPL';
 
   var args = a || {};
   args.__tishadowContainer = container;
@@ -43,87 +50,88 @@ function prepareArgs(a) {
   return args;
 }
 
-var create = function(fn,a) {
+var create = function (fn, a) {
   var args = prepareArgs(a);
   var o = fn(args);
   o.addEventListener('open', stack);
-  o.addEventListener('close', function(e) {
+  o.addEventListener('close', function (e) {
     unstack({ app: args.__tishadowApp, container: args.__tishadowContainer });
   });
-  if (require("/api/TiShadow").inspector) {
-    o.addEventListener('focus', function(e) {
+  if (require('/api/TiShadow').inspector) {
+    o.addEventListener('focus', function (e) {
       e.source._api = e.source.apiName;
-        log.inspect(e.source);
+      log.inspect(e.source);
     });
   }
   return o;
 };
 
 //Dumb objects are those we can't rely on any listeners for...
-var createDumb = function(fn,a) {
+var createDumb = function (fn, a) {
   var args = prepareArgs(a);
   args.__tishadowDumb = true;
 
   var o = fn(args);
-  stack({source: o});
+  stack({ source: o });
   return o;
 };
-var createHideable = function(fn, evt, a) {
+var createHideable = function (fn, evt, a) {
   var args = prepareArgs(a);
   var o = fn(args);
   o.__closeFn = 'hide';
-  stack({source: o});
+  stack({ source: o });
 
-  o.addEventListener(evt, function(e) {
+  o.addEventListener(evt, function (e) {
     unstack({ app: args.__tishadowApp, container: args.__tishadowContainer });
   });
   return o;
 };
 
-['createWindow', 'createTabGroup'].forEach(function(cmd) {
-  exports[cmd]= function(args) {
+['createWindow', 'createTabGroup'].forEach(function (cmd) {
+  exports[cmd] = function (args) {
     return create(Ti.UI[cmd], args);
   };
 });
 
-exports.createSplitWindow= function(args) {
+exports.createSplitWindow = function (args) {
   return create(Ti.UI.iPad.createSplitWindow, args);
 };
 
-exports.createNavigationWindow = function(args) {
-	return create(Ti.UI.iOS.createNavigationWindow,args);
+exports.createNavigationWindow = function (args) {
+  return create(Ti.UI.createNavigationWindow, args);
 };
 
-['createAlertDialog', 'createOptionDialog'].forEach(function(cmd) {
-  exports[cmd]= function(args) {
+['createAlertDialog', 'createOptionDialog'].forEach(function (cmd) {
+  exports[cmd] = function (args) {
     return createHideable(Ti.UI[cmd], 'click', args);
   };
 });
 
-exports.createPopover= function(args) {
+exports.createPopover = function (args) {
   return createHideable(Ti.UI.iPad.createPopover, 'hide', args);
 };
 
-exports.closeApp = function(a) {
-  var app = a || "__REPL";
+exports.closeApp = function (a) {
+  var app = a || '__REPL';
   if (app && containers[app]) {
-    _.keys(containers[app]).reverse().forEach(function(c) {
-      if (containers[app].hasOwnProperty(c)) {
-        var current = containers[app][c];
-        if (current.__tishadowDumb) {
-          unstack({app:app, container:c});
+    _.keys(containers[app])
+      .reverse()
+      .forEach(function (c) {
+        if (containers[app].hasOwnProperty(c)) {
+          var current = containers[app][c];
+          if (current.__tishadowDumb) {
+            unstack({ app: app, container: c });
+          }
+          log.debug('CLOSING: ' + c);
+          log.debug(current.apiName);
+          current[current.__closeFn || 'close']({ animated: false });
         }
-        log.debug("CLOSING: " + c);
-        log.debug(current.apiName);
-        current[current.__closeFn || 'close']({animated:false});
-      }
-    });
+      });
   }
   return;
 };
 
-exports.closeAll = function() {
-
+exports.closeAll = function () {
   for (var app in containers) {
     containers.hasOwnProperty(app) && exports.closeApp(app);
   }
